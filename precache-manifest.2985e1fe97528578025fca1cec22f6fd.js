94b9a8306fa9dc644a9db0cb5aa7d94c441b48f7
shakeeb <shakeebanwar250@gmail.com>
2020-09-27 21:23:08 +0500

    my first react app

---
+++ precache-manifest.2985e1fe97528578025fca1cec22f6fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50571aa2655c79c8d38aaf94ed5385fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "509a7922335995990342",
+    "url": "/static/css/main.d90d51c0.chunk.css"
+  },
+  {
+    "revision": "f43b662a326ae46b19c1",
+    "url": "/static/js/2.20f1630d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.20f1630d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509a7922335995990342",
+    "url": "/static/js/main.a96cfbff.chunk.js"
+  },
+  {
+    "revision": "7a1c4ee6b2728ac05991",
+    "url": "/static/js/runtime-main.f65ea6a2.js"
+  }
+]);
